b69733ee80549de1cbca0aa534c3a1930690fc8d
Som&soft <som8soft@gmail.com>
2021-10-12 23:09:30 +0300

    Add Reverse

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
-// concat
+// variables
 const array1 = [1, 2, 3];
 const array2 = [4, 5, 6];
-let result = array1.concat(array2);
+let result;
+
+// concat
+result = array1.concat(array2);
 console.log(result);
 
+// reverse
+result = array1.reverse();
+console.log(result);
