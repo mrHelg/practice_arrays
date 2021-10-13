a06493864fec09e6b998e21f3383ca589cc2f879
Som&soft <som8soft@gmail.com>
2021-10-13 21:16:05 +0300

    Add function2

--- js/index2.js
+++ js/index2.js
@@ -1,3 +1,8 @@
 // 1
-const hasElem = (arr, str) => arr.includes(str);
-console.log(hasElem(['abc','str', '1'], 'abc'));
+const hasElem = (arr, item) => arr.includes(item);
+
+//2
+const arr2 = [2, 5, 9, 11, 15];
+const number = 52;
+console.log(hasElem(arr2, number));
+
