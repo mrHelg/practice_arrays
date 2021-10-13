856409eff209ca4d6084f19944b906348e287589
Som&soft <som8soft@gmail.com>
2021-10-13 21:58:11 +0300

    Add function5

--- js/index2.js
+++ js/index2.js
@@ -24,11 +24,17 @@ const arr3 = [1, 2, 3, 3];
 // 4
 const MIN_VALUE = 15;
 const MAX_VALUE = 78;
-const myRandom = (max, min) => Math.floor(Math.random() * (max - min + 1) + min);
+const myRandom = (max, min) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
 const arr4 = [];
 for (let i = 0; i < 10; i++) {
   arr4.push(myRandom(MAX_VALUE, MIN_VALUE));
 }
-console.log(arr4);
-  
+// console.log(arr4);
 
+// 5
+const getMaxOfArray = (arr) => Math.max.apply(null, arr);
+const getMinOfArray = (arr) => Math.min.apply(null, arr);
+const arr5 = [4, -2, 5, 19, -130, 0, 10];
+console.log(getMaxOfArray(arr5));
+console.log(getMinOfArray(arr5));
